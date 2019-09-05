7d85f6ddc85746c154fe96e7f7384363aad34d49
Ajay <vergilssj@gmail.com>
2019-09-05 14:31:18 +0800

    Make App run inside docker

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-placeholder
+#!/bin/sh
+docker-compose build
+
+docker-compose down
+
+docker-compose up
+
+docker-compose exec go-blog psql -U admin -d go_blog -a -f /docker-entrypoint-initdb.d/db.sql
